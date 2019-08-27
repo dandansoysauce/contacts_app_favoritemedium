9a0dbabc6bcc0f955ca1dd2946727f9433ff62d1
Dani Ona <o.dani@keemail.me>
2019-08-28 04:04:46 +0800

    open dialog function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+
+import { AddContactDialog } from '../addcontact/addcontact.dialog';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(public dialog: MatDialog) {
+
+  }
+
+  addContact() {
+    this.openDialog(AddContactDialog);
+  }
+
+  private openDialog(dialogComponent: any) {
+    this.dialog.open(dialogComponent, {
+      width: '600px'
+    });
+  }
 }
