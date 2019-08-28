a2e232d4a6b7073b070eaa7774196849e6ac5a37
Dani Ona <o.dani@keemail.me>
2019-08-29 02:07:55 +0800

    implement formgroup to add contact dialog

--- src/app/addcontact/addcontact.dialog.ts
+++ src/app/addcontact/addcontact.dialog.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-addcontact-dialog',
     templateUrl: './addcontact.dialog.html',
     styleUrls: ['./addcontact.dialog.scss']
   })
-export class AddContactDialog {
+export class AddContactDialog implements OnInit {
+  newContactFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.newContactFormGroup = this._formBuilder.group({
+      name: [undefined, [Validators.required]],
+      email: [undefined, [Validators.required, Validators.email]],
+      phone: ['', Validators.pattern('[0-9]{0,10}')]
+    });
+  }
 }
