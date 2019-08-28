2ec1ae93f06fb06aa7102c9327c3384404012ea5
Dani Ona <o.dani@keemail.me>
2019-08-29 04:30:37 +0800

    contactslist: favorite contact function

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -10,13 +10,14 @@ import { AddEditContactDialog } from '../addeditcontact/addeditcontact.dialog';
     styleUrls: ['./contacts.component.scss']
   })
 export class ContactsComponent {
-  contacts: Contact[];
+  otherContacts: Contact[];
+  faveContacts: Contact[];
 
-  constructor(contactService: ContactService, private dialog: MatDialog) {
-    this.contacts = contactService.contacts;
+  constructor(private contactService: ContactService, private dialog: MatDialog) {
+    this.filterContacts(contactService.contacts);
 
     contactService.allContacts.subscribe((nextValue: any[]) => {
-      this.contacts = nextValue;
+      this.filterContacts(nextValue);
     });
   }
 
@@ -25,5 +26,15 @@ export class ContactsComponent {
       width: '600px',
       data: contact
     });
+  }
+
+  favoriteThisContact(contact: Contact) {
+    contact.Favorite = !contact.Favorite;
+    this.contactService.contacts = contact;
+  }
+
+  private filterContacts(contacts: Contact[]) {
+    this.faveContacts = contacts.filter(x => x.Favorite)
+    this.otherContacts = contacts.filter(x => !x.Favorite)
   }
 }
